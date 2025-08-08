4d0e822f57012993c83089007e2af16eca167db1
Adam Achebe <mister363636@gmail.com>
2025-08-08 16:44:33 -0700

    Model version

--- ltx_service.py
+++ ltx_service.py
@@ -12,8 +12,8 @@ def _round_to_vae(height: int, width: int, pipe) -> Tuple[int, int]:
 class LTXService:
     def __init__(
         self,
-        base_model: str = "Lightricks/LTX-Video-0.9.8-dev",
-        upsampler_model: str = "Lightricks/ltxv-spatial-upscaler-0.9.8",
+        base_model: str = "Lightricks/LTX-Video-0.9.7-dev",
+        upsampler_model: str = "Lightricks/ltxv-spatial-upscaler-0.9.7",
         device: str = "cuda",
         dtype = torch.bfloat16,
     ):
